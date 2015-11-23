0cdab3357fc207a5b92e43ce2e43421cba15da92
Tony Kelman <tony@kelman.net>
2015-11-22 17:52:04 -0800

    Don't add command line git to path in Windows juliarc
    no longer needed, can cause issues with mintty

--- contrib/windows/juliarc.jl
+++ contrib/windows/juliarc.jl
@@ -1,4 +1,3 @@
-# This file is a part of Julia. License is MIT: http://julialang.org/license
+# This file should contain site-specific commands to be executed on Julia startup
+# Users should store their own personal commands in homedir(), in a file named .juliarc.jl
 
-ENV["PATH"] = JULIA_HOME*";"*joinpath(JULIA_HOME,"..","Git","bin")*";"*
-    joinpath(JULIA_HOME,"..","Git","usr","bin")*";"*ENV["PATH"]
